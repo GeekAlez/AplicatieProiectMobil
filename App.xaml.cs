39b2d86f0daa85dd00ca54fb368b2adc8ae8984f
Ginga Alexandra <alexandra.ginga@stud.ubbcluj.ro>
2023-12-12 14:41:11 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AplicatieProiectMobil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
