8ef305d7233f7d7f0ba4defb33099da2236bb800
Ginga Alexandra <alexandra.ginga@stud.ubbcluj.ro>
2024-02-14 21:54:04 +0200

    10 pm

--- RezervarePage.xaml.cs
+++ RezervarePage.xaml.cs
@@ -6,4 +6,10 @@ public partial class RezervarePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    public async void OnTrimiteClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Ok", "Mulțumim pentru rezervare! Veți primi un e-mail în legătură cu aceasta.", "OK");
+    }
+
 }
